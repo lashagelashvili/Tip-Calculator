e59a280d4310ba0d24d43f4394e88f0527315bd7
lashagelashvili <las.gelashvili@gmail.com>
2021-11-04 23:56:58 +0400

    built ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TipCalculatorComponent } from './tip-calculator/tip-calculator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TipCalculatorComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/tip-calculator/tip-calculator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tip-calculator',
+  templateUrl: './tip-calculator.component.html',
+  styleUrls: ['./tip-calculator.component.css']
+})
+export class TipCalculatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
